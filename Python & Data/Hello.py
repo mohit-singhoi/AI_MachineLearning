6d1d41ea41d75deef176ca3877bca5c53c5d8bf3
Mohit kumar <mohitsinghoi501@gmail.com>
2026-01-26 15:22:20 +0530

    Initial commit

---
+++ Python & Data/Hello.py
@@ -0,0 +1,1 @@
+print("Hello, Welcome to AI & ML Era world!")
---
+++ Python & Data/Variables & Operators/Sum.py
@@ -0,0 +1,17 @@
+# Sum of two numbers 
+a= 10;
+b = 20;
+sum = a + b;
+print("The sum of a and b is:", sum);
+
+
+# This program calculates the sum of two numbers and prints the result.
+# You can change the values of a and b to compute the sum of different numbers.
+# Example:
+# If a = 5 and b = 15, the output will be:
+# The sum of a and b is: 20
+# Note: In Python, semicolons at the end of statements are optional.
+# The '+' operator is used to add two numbers.
+# The print function is used to display the output on the screen.
+print("Hello, Welcome to AI & ML Era world!")
+
