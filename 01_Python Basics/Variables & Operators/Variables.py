ab1d6c0207892f85421688114a02f5383f6dad6a
Mohit kumar <mohitsinghoi501@gmail.com>
2026-01-27 20:33:58 +0530

    Variables in Python

---
+++ 01_Python Basics/Variables & Operators/Variables.py
@@ -0,0 +1,34 @@
+# 1. Integer variable
+age = 21
+print("Age:", age)
+print("Type of age:", type(age))
+
+# 2. Float variable
+height = 5.7
+print("Height:", height)
+print("Type of height:", type(height))
+
+# 3. String variable
+name = "Mohit Kumar"
+print("Name:", name)
+print("Type of name:", type(name))
+
+# 4. Boolean variable
+is_student = True
+print("Is Student:", is_student)
+print("Type of is_student:", type(is_student))
+
+# 5. Complex variable
+complex_number = 3 + 4j
+print("Complex Number:", complex_number)
+print("Type of complex_number:", type(complex_number))
+
+# This program demonstrates different types of variables in Python.
+# You can change the values of these variables to see how their types remain consistent.
+# The type() function is used to determine the data type of a variable.
+# The print() function is used to display the variable values and their types on the screen.
+print("Hello, Welcome to AI & ML Era world!")
+# This program demonstrates different types of variables in Python.
+# You can change the values of these variables to see how their types remain consistent.
+# The type() function is used to determine the data type of a variable.
+# The print() function is used to display the variable values and their types on the screen.
